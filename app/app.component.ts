3de7cd9a3fd1c0fe00f33c2f2e85f71ae0e0b4a8
Calla Rudolph <calla@>
2017-08-14 16:02:06 -0700

    add viewRecipe method with button to show selectedRecipe details

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,20 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Recipe Box!!!</h1><br><br>
-    <h3><ul class="list-unstyled" *ngFor="let currentRecipe of recipes"><div id="title">{{currentRecipe.title}}</div>
-    <br>
-      <h4>Ingredients:</h4>
-      <ul class *ngFor="let currentIngredient of currentRecipe.ingredients">
-        <li id="ingredient"><h5>{{currentIngredient}}</h5></li>
-      </ul>
-      <h4>INSTRUCTIONS:</h4>
-      <ul class *ngFor="let currentDirection of currentRecipe.directions">
-        <li id="direction"><h5>{{currentDirection}}</h5></li>
-      </ul>
+    <ul class="list-unstyled" *ngFor="let currentRecipe of recipes"><h3><div id="title">{{currentRecipe.title}}</div></h3><button (click)="viewRecipe(currentRecipe)">View Recipe</button>
       <br>
-    </ul></h3>
+      <div *ngIf="selectedRecipe">
+        <h4>Ingredients:</h4>
+        <ul class *ngFor="let currentIngredient of currentRecipe.ingredients">
+          <li id="ingredient"><h5>{{currentIngredient}}</h5></li>
+        </ul>
+        <h4>INSTRUCTIONS:</h4>
+        <ul class *ngFor="let currentDirection of currentRecipe.directions">
+          <li id="direction"><h5>{{currentDirection}}</h5></li>
+        </ul>
+        <br>
+      </div>
+    </ul>
   </div>
   `
 })
@@ -26,6 +28,11 @@ export class AppComponent {
     new Recipe('Brownies', ['Flour', 'chocolate', 'sprinkles', 'butter'], ['mix together', 'eat']),
     new Recipe('Pizza', ['Crust', 'chicken', 'sauce', 'CHEESE'], ['throw final three ingredients on crust', 'bake in oven', 'wait five minutes', 'eat'])
   ];
+  selectedRecipe = null;
+
+  viewRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
 }
 
 export class Recipe {
